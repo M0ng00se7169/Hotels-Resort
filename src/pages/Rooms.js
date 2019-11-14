29ed3d74a531f4cc2f93ec9ed2b1262813464121
M0ng00se7169 <NickKomarowski@gmail.com>
2019-11-14 13:41:55 +0100

    Added filter room techniques

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom';
 
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import RoomsContainer from '../components/RoomsContainer'
 
 const Rooms = () => {
-  return <Hero hero="roomsHero">
-    <Banner title="our rooms">
-      <Link to="/" className="btn-primary">return home</Link>
-    </Banner>
-  </Hero>
+  return (
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">return home</Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </>
+  )
 }
 
 export default Rooms;
