4c9835f96382aa75eb66a995559f8a4ddcb80472
M0ng00se7169 <NickKomarowski@gmail.com>
2019-11-21 17:55:08 +0100

    Deploying to Netlify

--- src/context.js
+++ src/context.js
@@ -29,7 +29,7 @@ export default class RoomProvider extends Component {
     try {
       let response = await Client.getEntries({
         content_type: "beachResortRoom",
-        order: '-fields.price'
+        order: "-fields.price"
       });
       let rooms = this.formatData(response.items);
       let featuredRooms = rooms.filter(room => room.featured === true);
